3c84f00a74b499671054d028201b604ed0e57cf9
kalpadiptya3raredynamics <kalpadiptya.roy@3raredynamics.com>
2023-09-21 14:35:46 +0530

    Remove unused imports

--- src/main/java/com/threeraredyn/campbooka/controller/PropertyController.java
+++ src/main/java/com/threeraredyn/campbooka/controller/PropertyController.java
@@ -2,7 +2,6 @@ package com.threeraredyn.campbooka.controller;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- src/main/java/com/threeraredyn/campbooka/serviceimpl/PropertyServiceImpl.java
+++ src/main/java/com/threeraredyn/campbooka/serviceimpl/PropertyServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.threeraredyn.campbooka.entity.Places;
@@ -98,7 +96,7 @@ public class PropertyServiceImpl implements PropertyService {
 
     @Override
     public void addNewProperties(List<PropertyRequestDTO> propertyRequestDTOList) {
-        
+
         List<Property> propertyList = propertyRequestDTOList.stream()
             .filter(prop -> !checkAlreadyExists(prop.getPropertyName(), prop.getPlaceName()))
             .map( prop -> {
